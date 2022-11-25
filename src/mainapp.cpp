3d5bd555878f8d8abdbf453fbd47b7433b614c95
Glitch752 <xxGlitch752xx@gmail.com>
2022-11-24 18:20:27 -0600

    Added `Bind`s to every menu action

--- src/mainapp.cpp
+++ src/mainapp.cpp
@@ -1,7 +1,7 @@
 #include "./mainapp.h"
 #include <wx/wx.h>
 
-MainApp::MainApp(const wxString& title) : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(250, 150)) {
+MainApp::MainApp(const wxString& title) : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(800, 560)) {
     wxImage::AddHandler(new wxXPMHandler);
 
     wxIcon icon(wxT("./tempIcon.xpm"), wxBITMAP_TYPE_XPM);
@@ -37,4 +37,77 @@ MainApp::MainApp(const wxString& title) : wxFrame(NULL, wxID_ANY, title, wxDefau
     menuBar->Append(helpMenu, wxT("&Help"));
 
     SetMenuBar(menuBar);
+
+
+    Bind(wxEVT_MENU, &MainApp::OnNew,       this, wxID_NEW      );
+    Bind(wxEVT_MENU, &MainApp::OnOpen,      this, wxID_OPEN     );
+    Bind(wxEVT_MENU, &MainApp::OnSave,      this, wxID_SAVE     );
+    Bind(wxEVT_MENU, &MainApp::OnSaveAs,    this, wxID_SAVEAS   );
+    Bind(wxEVT_MENU, &MainApp::OnExit,      this, wxID_EXIT     );
+    Bind(wxEVT_MENU, &MainApp::OnUndo,      this, wxID_UNDO     );
+    Bind(wxEVT_MENU, &MainApp::OnRedo,      this, wxID_REDO     );
+    Bind(wxEVT_MENU, &MainApp::OnCut,       this, wxID_CUT      );
+    Bind(wxEVT_MENU, &MainApp::OnCopy,      this, wxID_COPY     );
+    Bind(wxEVT_MENU, &MainApp::OnPaste,     this, wxID_PASTE    );
+    Bind(wxEVT_MENU, &MainApp::OnDelete,    this, wxID_DELETE   );
+    Bind(wxEVT_MENU, &MainApp::OnSelectAll, this, wxID_SELECTALL);
+    Bind(wxEVT_MENU, &MainApp::OnAbout,     this, wxID_ABOUT    );
+}
+
+void MainApp::OnOpen(wxCommandEvent& event) {
+    wxFileDialog openFileDialog(this, _("Open file"), "", "", "Any file|*", wxFD_OPEN|wxFD_FILE_MUST_EXIST);
+
+    if (openFileDialog.ShowModal() == wxID_CANCEL) {
+        return;
+    }
+
+    // TODO: Implement
+}
+
+void MainApp::OnSave(wxCommandEvent& event) {
+
+}
+
+void MainApp::OnSaveAs(wxCommandEvent& event) {
+
+}
+
+void MainApp::OnExit(wxCommandEvent& event) {
+    Close(true);
+}
+
+void MainApp::OnUndo(wxCommandEvent& event) {
+
+}
+
+void MainApp::OnRedo(wxCommandEvent& event) {
+
+}
+
+void MainApp::OnCut(wxCommandEvent& event) {
+
+}
+
+void MainApp::OnCopy(wxCommandEvent& event) {
+
+}
+
+void MainApp::OnPaste(wxCommandEvent& event) {
+
+}
+
+void MainApp::OnDelete(wxCommandEvent& event) {
+
+}
+
+void MainApp::OnSelectAll(wxCommandEvent& event) {
+
+}
+
+void MainApp::OnAbout(wxCommandEvent& event) {
+    wxMessageBox("A simple text editor to learn about GUI applications in C++.", "About", wxOK | wxICON_INFORMATION);
+}
+
+void MainApp::OnNew(wxCommandEvent& event) {
+    
 }
--- src/mainapp.h
+++ src/mainapp.h
@@ -3,4 +3,20 @@
 class MainApp : public wxFrame {
     public:
         MainApp(const wxString& title);
+    protected:
+        void OnNew(wxCommandEvent& event);
+        void OnOpen(wxCommandEvent& event);
+        void OnSave(wxCommandEvent& event);
+        void OnSaveAs(wxCommandEvent& event);
+        void OnExit(wxCommandEvent& event);
+
+        void OnUndo(wxCommandEvent& event);
+        void OnRedo(wxCommandEvent& event);
+        void OnCut(wxCommandEvent& event);
+        void OnCopy(wxCommandEvent& event);
+        void OnPaste(wxCommandEvent& event);
+        void OnDelete(wxCommandEvent& event);
+        void OnSelectAll(wxCommandEvent& event);
+
+        void OnAbout(wxCommandEvent& event);
 };
